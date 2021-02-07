3db006a7903e6bcfdd4ed39ead40582d478225e8
Berk <Berk@BERK>
2021-02-07 20:10:53 +0300

    Add project files.

---
+++ askUyumu/askUyumu.py
@@ -0,0 +1,33 @@
+loop = True
+while loop == True:
+    try:
+        import random as r
+
+        qstn = "e"
+        while qstn == "e":
+            male = input("Erkek: ")
+            female = input("Kadın: ")
+            percent = r.randint(1, 100)
+            if male == "berk" and female == "buse":
+                print("Aşk Uyumunuz: Ölçülemeyecek Kadar Yüksek")
+                qstn = input("Tekrar Denemek İster Misiniz? (e/h): ")
+                if qstn == "h":
+                    loop = False
+                while qstn != "e" and qstn != "h":
+                    print("Yanlış Komut Girdiniz!")
+                    qstn = input("Tekrar Denemek İster Misiniz? (e/h): ")
+                    if qstn == "h":
+                        loop = False
+            else:
+                print("Aşk Uyumunuz: %", percent)
+                qstn = input("Tekrar Denemek İster Misiniz? (e/h): ")
+                if qstn == "h":
+                    loop = False
+                while qstn != "e" and qstn != "h":
+                    print("Yanlış Komut Girdiniz!")
+                    qstn = input("Tekrar Denemek İster Misiniz? (e/h): ")
+                    if qstn == "h":
+                        loop = False
+    except:
+        print("Hata Oluştu!")
+        loop = True
